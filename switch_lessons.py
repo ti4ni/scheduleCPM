9ab04831db5c1b08f5237e1642bafab9eb327c0f
ti4ni <112348652+ti4ni@users.noreply.github.com>
2023-04-19 00:47:50 +0300

    Update switch_lessons.py

--- switch_lessons.py
+++ switch_lessons.py
@@ -1,6 +1,6 @@
 import pymysql
 
-try:  # подключение к бд
+try:  # Подключение к бд
     connection = pymysql.connect(
         host="localhost",
         port=3306,
@@ -15,27 +15,23 @@ try:  # подключение к бд
         try:
             with connection.cursor() as cursor:  # Смена двух пар местами между собой. Все работает, осталось лишь
                 # передавать значения в переменную (сейчас стоят тестовые)
-                selected_lesson1 = f"SELECT `{lesson1}` FROM `класс`;"
-                selected_lesson2 = f"SELECT `{lesson2}` FROM `класс`;"
-                cursor.execute(selected_lesson1)
-                str_selected_lesson1 = str(cursor.fetchall())
-                cursor.execute(selected_lesson2)
-                str_selected_lesson2 = str(cursor.fetchall())
-                pre_done_selected_lesson1 = str_selected_lesson1.replace("(('", "")
-                pre_done_selected_lesson2 = str_selected_lesson2.replace("(('", "")
-                done_selected_lesson1 = pre_done_selected_lesson1.replace("',),)", "")  # значение в виде строки из
-                # Понедельник1 из БД
-                done_selected_lesson2 = pre_done_selected_lesson2.replace("',),)", "")  # значение в виде строки из
-                # Понедельник2 из БД
-                switching_lessons1 = f"UPDATE класс SET `{lesson1}` = '{done_selected_lesson2}' WHERE `Класс` = '{klass}';"
-                cursor.execute(switching_lessons1)
-                switching_lessons2 = f"UPDATE класс SET `{lesson2}` = '{done_selected_lesson1}' WHERE `Класс` = '{klass}';"
-                cursor.execute(switching_lessons2)
+                bd_selected_lesson1 = f"SELECT `{lesson1}` FROM `класс` WHERE `Класс` = '{klass}';"  # 1 урок из БД
+                bd_selected_lesson2 = f"SELECT `{lesson2}` FROM `класс` WHERE `Класс` = '{klass}';"  # 2 урок из БД
+                cursor.execute(bd_selected_lesson1)
+                selected_lesson1 = str(cursor.fetchall()).replace("(('", "").replace("',),)", "")  # 2 урок из БД в виде
+                # строки
+                cursor.execute(bd_selected_lesson2)
+                selected_lesson2 = str(cursor.fetchall()).replace("(('", "").replace("',),)", "")  # 2 урок из БД в виде
+                # строки
+                switching_lessons1 = f"UPDATE класс SET `{lesson1}` = '{selected_lesson2}' WHERE `Класс` = '{klass}';"
+                cursor.execute(switching_lessons1)  # SQL запрос на смену 1 урока
+                switching_lessons2 = f"UPDATE класс SET `{lesson2}` = '{selected_lesson1}' WHERE `Класс` = '{klass}';"
+                cursor.execute(switching_lessons2)  # SQL запрос на смену 2 урока
                 connection.commit()
 
-        finally:  # закрываем подключение
+        finally:  # Закрываем подключение к базе данных
             connection.close()
-    switching("Понедельник1", "Понедельник2", "11Л")
+    switching("Понедельник1", "Понедельник2", "11Л")  # Вызываем функцию
 
-except Exception as ex:  # вывод ошибок
+except Exception as ex:  # Вывод ошибок
     print(ex)
